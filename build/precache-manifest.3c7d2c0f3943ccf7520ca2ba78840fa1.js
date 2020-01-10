cfeda4595f7b5b598c13079720b41fb0317bce0a
Ron Dayan <Ron13dayan@gmail.com>
2020-01-10 12:32:15 +0200

    strange files

---
+++ build/precache-manifest.3c7d2c0f3943ccf7520ca2ba78840fa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d42e6f832347c9ab6343e91ad604644",
+    "url": "/Ronchito13/robots/index.html"
+  },
+  {
+    "revision": "f89c68362a41e1e376c6",
+    "url": "/Ronchito13/robots/static/css/main.7fad2709.chunk.css"
+  },
+  {
+    "revision": "e2b5df07dba2a261ac9d",
+    "url": "/Ronchito13/robots/static/js/2.548194fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Ronchito13/robots/static/js/2.548194fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f89c68362a41e1e376c6",
+    "url": "/Ronchito13/robots/static/js/main.2c61fd38.chunk.js"
+  },
+  {
+    "revision": "50c40d315cc2a409c3e4",
+    "url": "/Ronchito13/robots/static/js/runtime-main.60d2fc91.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Ronchito13/robots/static/media/SEGA.82e09f78.TTF"
+  }
+]);
